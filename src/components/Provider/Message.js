04ae576cf6fa92aefee6db76834f305e99ba5478
Joaquin Diaz <joaquin.diaz@ies.cl>
2023-08-08 04:44:01 +0000

    react ^16.14.0

--- src/components/Provider/Message.js
+++ src/components/Provider/Message.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import autobind from 'autobind-decorator'
 import PropTypes from 'prop-types'
 
